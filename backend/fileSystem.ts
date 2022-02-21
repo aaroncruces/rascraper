ea4b6dfb6b53ce26c917016e4320cdbb8ddc9baa
aaron cruces <aaroncruces@gmail.com>
2022-02-21 19:59:01 -0300

    trying mocking accessing files

--- backend/fileSystem.ts
+++ backend/fileSystem.ts
@@ -2,23 +2,48 @@ import { GameSystem, GameSystems } from "./structures/GameSystems";
 const fs = require("fs").promises;
 const path = require("path");
 
-export const createFolder = async (folderpath: string) => {
+export const text = (string: string) => {
+  console.log(string);
+  return true;
+};
+
+export const createFolder = async (folderpath: string): Promise<boolean> => {
   await fs.mkdir(folderpath, { recursive: true });
   const stat = await fs.stat(folderpath);
   return stat.isDirectory();
 };
 
-export const deleteFolder = async (folderpath: string) => {
-  await fs.rm(folderpath, { recursive: true, force: true });
+export const deleteFileOrFolder = async (
+  itemRoute: string
+): Promise<boolean> => {
+  await fs.rm(itemRoute, { recursive: true, force: true });
   try {
-    await fs.access(folderpath);
+    await fs.access(itemRoute);
     return false;
   } catch {
     return true;
   }
 };
 
-export const createTextFile = () => {};
+export const createTextFileFromObject = async (
+  fileRoute: string,
+  objectToBeWritten: object
+): Promise<boolean> => {
+  await fs.writeFile(fileRoute, JSON.stringify(objectToBeWritten, null, 2));
+  try {
+    await fs.access(fileRoute);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const readTextFileAsObject = async (
+  fileRoute: string
+): Promise<object> => {
+  const textResult = await fs.readFile(fileRoute);
+  return JSON.parse(textResult);
+};
 
 export const gameSystemFromRomFolderRoute = (
   fileRoute: string
--- jestTests/fileSystem.test.ts
+++ jestTests/fileSystem.test.ts
@@ -1,6 +1,32 @@
-import { createFolder, deleteFolder } from "../backend/fileSystem";
+import {
+  createFolder,
+  createTextFileFromObject,
+  deleteFileOrFolder,
+  readTextFileAsObject,
+} from "../backend/fileSystem";
 
+const globalAccess: any = global;
+
+jest.mock("../backend/fileSystem", () => {
+  const mockCreateFolder = jest.fn(async (folderRoute: string) => {
+    console.log("mockCreateFolder");
+    if (!globalAccess.globalFSMock) globalAccess.globalFSMock = {};
+    let globalFSMock = globalAccess.globalFSMock;
+    //todo: insert the "folder" in globalFSMock
+    return true;
+  });
+  const originalModule = jest.requireActual("../backend/fileSystem.ts");
+  return {
+    __esModule: true,
+    ...originalModule,
+    createFolder: mockCreateFolder,
+  };
+});
 describe.skip("temporarily skipping already tested: create and delete folders and text files", () => {
+  //todo: mock this when tested
+  beforeAll(() => {
+    //todo: delete all folders and files in ./testsandbox
+  });
   it("creates a folder  called jestfoldertest in ./testsandbox", async () => {
     await expect(
       createFolder("./testsandbox/jestfoldertest")
@@ -15,29 +41,57 @@ describe.skip("temporarily skipping already tested: create and delete folders an
 
   it("deletes the folder created", async () => {
     await expect(
-      deleteFolder("./testsandbox/jestfoldertest")
+      deleteFileOrFolder("./testsandbox/jestfoldertest")
     ).resolves.toBeTruthy();
   });
 
-  it("deletes a nonexistant folder", async () => {
+  it("deletes a nonexistant folder or file to try idempotency", async () => {
     await expect(
-      deleteFolder("./testsandbox/somefolderNonExistant")
+      deleteFileOrFolder("./testsandbox/somefolderNonExistant")
+    ).resolves.toBeTruthy();
+  });
+
+  let testObjectToBeWritten = { hello: "World", hola: "mundo" };
+  it("creates a text file named jestfiletest.json with a json object", async () => {
+    await expect(
+      createTextFileFromObject(
+        "./testsandbox/jestfiletest.json",
+        testObjectToBeWritten
+      )
+    ).resolves.toBeTruthy();
+  });
+  it("reads the text file previously created (jestfiletest.json) and gives the result as object", async () => {
+    await expect(
+      readTextFileAsObject("./testsandbox/jestfiletest.json")
+    ).resolves.toEqual(testObjectToBeWritten);
+  });
+
+  it("Creates the same file to test idempotency", async () => {
+    await expect(
+      createTextFileFromObject(
+        "./testsandbox/jestfiletest.json",
+        testObjectToBeWritten
+      )
+    ).resolves.toBeTruthy();
+  });
+
+  it("deletes file created (jestfiletest.json)", async () => {
+    await expect(
+      deleteFileOrFolder("./testsandbox/jestfiletest.json")
     ).resolves.toBeTruthy();
   });
 });
 
 describe("create and delete folders and text files", () => {
-  /*
-  it("", () => {
+  it("creates a folder  called jestfoldertest in ./testsandbox", async () => {
+    await expect(
+      createFolder("./testsandbox/jestfoldertest")
+    ).resolves.toBeTruthy();
+  });
 
-  });
-  */
-  it("creates a text file named jestfiletest.json with a json object", () => {
-    const testObjectToBeWritten = { hello: "world", hola: "mundo" };
-    //test writting nothing
-    //write object
-    //read objct
-  });
+  //test writting nothing
+  //write object
+  //read objct
   //it
   //it creates a folder named jestfilefolder having a file with the same name
   //it does the same thing with a file
