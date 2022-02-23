6a09e4fa544bf89655838aa987b5d877949719d7
aaron cruces <aaroncruces@gmail.com>
2022-02-23 17:17:30 -0300

    drawing an schematic of how coud be the app

--- app.ts
+++ app.ts
@@ -6,25 +6,4 @@ import { createFolder } from "./backend/fileSystem";
 var scraper = require("./backend/scraper");
 //scraper.scrapeRom({ engine: "screenscraper", filename: "mslug.zip" });
 const testFolder = "./romsandbox";
-//scraper.scrapeFolder(testFolder);
-
-//createFolder(testFolder + "/created/");
-const jest = require("jest");
-
-const run = async () => {
-  try {
-    const jestOptions = {
-      projects: [__dirname],
-      silent: true,
-    };
-    const jestResult = await jest.runCLI(jestOptions, jestOptions.projects);
-    console.log(jestResult);
-
-    //if jest results ok. run rest of things
-    if (true) {
-    }
-  } catch (error) {
-    console.error("failure");
-  }
-};
-run();
+scraper.scrapeFolder(testFolder);
